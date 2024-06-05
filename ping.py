30efa961cc419ed63f46ac7196d478f82ecbd66a
Peyton <65147510+Erkite@users.noreply.github.com>
2024-06-05 11:53:05 -0700

    Add files via upload

---
+++ ping.py
@@ -0,0 +1,24 @@
+import platform
+import subprocess
+
+def ping(host, count):
+
+    if platform.system().lower()=='windows': 
+        parameter = '-n'
+    else:
+        parameter = '-c'
+
+    command = ['ping', parameter, str(count), host]
+
+    return subprocess.call(command) == 0
+
+def getInput():
+    try:
+        return int(input("Enter an integer of how many packets you want to send: "))
+    except:
+        return getInput()
+
+host_name = input("Enter a host/IP: ")
+count = getInput()
+
+ping(host_name, count)
